9e52ce218e7c7e7c0c07380a058dcb7d251baa27
Bento Netto <bentoeemanuele@gmail.com>
2024-06-26 22:11:29 -0300

    Implementação da Exclusão de aula para instrutor

--- src/controladores/aulas.ts
+++ src/controladores/aulas.ts
@@ -1,6 +1,7 @@
 import { Request, Response} from "express";
 import bancodedados from '../bancodedados'
 
+//Declaração de função para cadastro de aula para um instrutor, vai utilizar o método PUSH
 export function cadastrarAulas(req: Request, res: Response){
     const { id } = req.params
     const { nome } = req.body
@@ -22,4 +23,31 @@ export function cadastrarAulas(req: Request, res: Response){
         return res.status(201).json(novaAula)
     }
     instrutor.aulas = [novaAula]
+}
+//Declaração de função para deletar cadastro de uma aula para determinado intrutor, vai utilizar o verbo DELETE
+export function excluirAula (req: Request, res: Response){
+    const { id, idAula } = req.params 
+    const instrutor = bancodedados.instrutores.find((item) => {
+        return item.id === Number(id) //Aqui se converte o ID de string para number
+    })
+    if(!instrutor){//verificação da existencia do instrutor informado na requisição
+        return res.status(404).json({
+            mensagem: 'Instrutor(a) não encontrado(a)!'
+        })
+    }
+    if(!instrutor.aulas){
+        return res.status(404).json({
+            mensagem: 'Aula não encontrada para o instrutor informado'
+        })
+    }
+    const aulaIndex = instrutor.aulas?.findIndex((item)=>{//atribuicao de variável para o caso de aula encontrada
+        return item.id === Number(idAula)
+    })
+    if(aulaIndex === -1){//verificação da existencia da aula informado na requisição
+        return res.status(404).json({
+            mensagem: 'Aula não encontrada para o instrutor informado'
+        })
+    }
+    instrutor.aulas.splice(aulaIndex,1) //uma vez que a aula tenha sido encontrada, exclui a mesma com o método splice
+    return res.status(204).send()//retorno No Content
 }
--- src/rotas.ts
+++ src/rotas.ts
@@ -1,7 +1,7 @@
 //IMPORTAÇÃO DE BIBLIOTECAS, ROTAS, INTERMEDIÁRIOS E CONTROLADORES
 import { Router } from 'express'
 import { atualizar, atualizarEmail, cadastrar, detalhar, excluir, listar } from './controladores/intrutores'
-import { cadastrarAulas } from './controladores/aulas'
+import { cadastrarAulas, excluirAula } from './controladores/aulas'
 
 const rotas = Router()
 
@@ -12,6 +12,6 @@ rotas.put('/instrutores/:id', atualizar)//editar um intrutor
 rotas.patch('/instrutores/:id/alterarEmail', atualizarEmail)//editar parcialmente um instrutor
 rotas.delete('/instrutores/:id', excluir)//excluir um instrutor
 rotas.post('/instrutores/:id/aulas', cadastrarAulas)//cadastrar uma aula para um instrutor
-//excluir uma aula para um instrutor
+rotas.delete('/instrutores/:id/aulas/:idAula', excluirAula)//excluir uma aula para um instrutor
 
 export default rotas
